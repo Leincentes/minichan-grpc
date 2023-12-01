007193dbee8b67044af6d7bc2363a4e3120f76d4
Niel <leincentes@gmail.com>
2023-12-02 00:42:29 +0800

    added

--- lib/Cli/ServeCommand.php
+++ lib/Cli/ServeCommand.php
@@ -11,16 +11,13 @@ class ServeCommand extends Command
     }
     public function handle($args)
     {
-                // Assuming the script is located in the 'Service' folder
-                $scriptPath = 'Services/Bootstrap/serve.php';
+        $scriptPath = 'Services/Bootstrap/serve.php';
 
-                // Check if the script exists
-                if (!file_exists($scriptPath)) {
-                    echo "Error: The script $scriptPath does not exist.\n";
-                    exit(1);
-                }
-        
-                // Run the script using the PHP CLI
-                system("php $scriptPath");
+        if (!file_exists($scriptPath)) {
+            echo "Error: The script $scriptPath does not exist.\n";
+            exit(1);
+        }
+
+        system("php $scriptPath");
     }
 }
