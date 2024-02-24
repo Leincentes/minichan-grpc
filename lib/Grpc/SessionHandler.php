72b41bd3e20b321aa5fe232fe49bece5fbdcbd58
Niel <leincentes@gmail.com>
2024-02-24 21:21:41 +0800

    handling client session

---
+++ lib/Grpc/SessionHandler.php
@@ -0,0 +1,78 @@
+<?php
+declare(strict_types=1);
+
+namespace Minichan\Grpc;
+
+class SessionHandler
+{
+    /**
+     * @var callable
+     */
+    protected $middleware;
+
+    /**
+     * @var callable
+     */
+    protected $idGenerator;
+
+    protected bool $useCookies;
+
+    protected bool $useOnlyCookies;
+
+    /**
+     * Inject dependencies
+     *
+     * @param callable $middleware function (\Swoole\Http\Request $request, \Swoole\Http\Response $response)
+     * @param callable $idGenerator
+     * @param bool|null $useCookies
+     * @param bool|null $useOnlyCookies
+     */
+    public function __construct(
+        callable $middleware,
+        $idGenerator = 'session_create_id',
+        ?bool $useCookies = null,
+        ?bool $useOnlyCookies = null
+    ) {
+        $this->middleware = $middleware;
+        $this->idGenerator = $idGenerator;
+        $this->useCookies = is_null($useCookies) ? (bool)ini_get('session.use_cookies') : $useCookies;
+        $this->useOnlyCookies = is_null($useOnlyCookies) ? (bool)ini_get('session.use_only_cookies') : $useOnlyCookies;
+    }
+
+    /**
+     * Delegate execution to the underlying middleware wrapping it into the session start/stop calls
+     */
+    public function __invoke(\Swoole\Http\Request $request, \Swoole\Http\Response $response)
+    {
+        $sessionName = session_name();
+        if ($this->useCookies && isset($request->cookie[$sessionName])) {
+            $sessionId = $request->cookie[$sessionName];
+        } else if (!$this->useOnlyCookies && isset($request->get[$sessionName])) {
+            $sessionId = $request->get[$sessionName];
+        } else {
+            $sessionId = call_user_func($this->idGenerator);
+        }
+        session_id($sessionId);
+        session_start();
+        if ($this->useCookies) {
+            $cookie = session_get_cookie_params();
+            $response->cookie(
+                $sessionName,
+                $sessionId,
+                $cookie['lifetime'] ? time() + $cookie['lifetime'] : 0,
+                $cookie['path'],
+                $cookie['domain'],
+                $cookie['secure'],
+                $cookie['httponly']
+            );
+        }
+        try {
+            call_user_func($this->middleware, $request, $response);
+        } finally {
+            session_write_close();
+            session_id('');
+            $_SESSION = [];
+            unset($_SESSION);
+        }
+    }
+}
