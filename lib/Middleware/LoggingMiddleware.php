b3761123fb9df1b40bc0cfc6cd1a7854c6fc1e64
Niel <leincentes@gmail.com>
2024-02-26 08:11:33 +0800

    modified logging

--- lib/Middleware/LoggingMiddleware.php
+++ lib/Middleware/LoggingMiddleware.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Minichan\Middleware;
 
-use Minichan\Config\Constant;
 use Minichan\Config\Status;
 use Minichan\Grpc\Util;
 
@@ -32,13 +31,8 @@ class LoggingMiddleware implements \Minichan\Middleware\MiddlewareInterface
         $ua = $rawRequest->header['user-agent'];
         $httpMethod = $rawRequest->server['request_method'];
         $requestUri = $rawRequest->server['request_uri'];
-        $requestHeaders = $rawRequest->header;
-        $requestPayload = $rawRequest->getContent();
 
-        // Log timestamp
         $timestamp = date('Y-m-d H:i:s');
-
-        // Continue handling the request in the middleware stack
         $startTime = microtime(true);
         $response = $handler->handle($request);
         $executionTime = microtime(true) - $startTime;
@@ -47,48 +41,16 @@ class LoggingMiddleware implements \Minichan\Middleware\MiddlewareInterface
         Util::log(
             Status::LOG,
             "{$timestamp} - GRPC request: {$client}->{$server}, stream({$streamId}), {$httpMethod} {$requestUri}, User-Agent: {$ua}, Execution Time: " . number_format($executionTime, 4) . " seconds");
-        
-        // print("=======================================================\n");
-        // // Log request headers without backslashes
-        // Util::log(Status::LOG, "Request Headers: " . stripslashes(json_encode($requestHeaders, JSON_PRETTY_PRINT)));
-        // print("=======================================================\n");
 
-        // // Log encrypted request payload if present
-        // if (!empty($requestPayload)) {
-        //     $encryptedPayload = $this->encryptPayload($requestPayload);
-        //     Util::log(Status::LOG, "Encrypted Request Payload: " . base64_encode($encryptedPayload));
-        // }
-        // print("=======================================================\n");
-
-
-
-        // // Log response status
-        // $responseStatus = $context->getValue(Constant::GRPC_STATUS);
-        // Util::log(Status::LOG, "Response Status: {$responseStatus}");
-        // print("=======================================================\n");
-
-        // // Log execution time
-        // Util::log(Status::LOG, "Execution Time: " . number_format($executionTime, 4) . " seconds");
-        // print("=======================================================\n");
-
+        // Clear terminal info after 1000 lines
+        static $lineCount = 0;
+        $lineCount++;
+        $clearTerminalAfterLines = 1000;
+        if ($lineCount >= $clearTerminalAfterLines) {
+            echo "\033[2J\033[H";
+            $lineCount = 0; 
+        }
+    
         return $response;
-    }
-
-    /**
-     * Encrypt the payload.
-     *
-     * @param string $payload
-     *
-     * @return string
-     */
-    private function encryptPayload(string $payload): string
-    {
-        // use a secure encryption algorithm and key management in a real-world scenario
-        $encryptionKey = 'test-encrypt';
-        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
-
-        $encryptedPayload = openssl_encrypt($payload, 'aes-256-cbc', $encryptionKey, 0, $iv);
-
-        return $iv . $encryptedPayload;
     }
 }
