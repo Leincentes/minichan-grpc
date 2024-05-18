1bb3404849f2125d3a2758aab3c0a6055b853b50
Leincentes <leincentes@gmail.com>
2024-05-19 02:44:05 +0800

    services: modified config added middleware registration

--- Services/Config/Config.php
+++ Services/Config/Config.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Services\Config;
 
+use Services\SampleService;
+
 /**
  * Configuration class for defining gRPC services.
  */
@@ -18,7 +20,14 @@ class Config
     {
         // Add your services here
         return [
-            
+            SampleService::class,
+        ];
+    }
+    public static function registerMiddlewares(): array 
+    {
+        // Add your middlewares here
+        return [
+
         ];
     }
 }
